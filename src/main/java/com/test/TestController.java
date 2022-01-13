5e29735d72ea73b5204b29afe0c161803c64d724
Abhishek Srivastava <abhi.srivastava7631@gmail.com>
2022-01-13 22:25:25 +0530

    springdevtool

---
+++ src/main/java/com/test/TestController.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	@RequestMapping("/")
+	@ResponseBody
+	public String test() {
+		int a = 31;
+		int b = 51;
+		return "this is just testing  sum of a and b is : "+(a+b) ;
+	}
+}
